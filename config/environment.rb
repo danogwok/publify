0c28b25a6a2548e0755c90cd3014c552b5df8600
Daniel Ogwok <danogwok@yahoo.co.uk>
2014-03-24 11:43:19 +0300

    deleted tls true

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,7 @@ Publify::Application.initialize!
  ActionMailer::Base.delivery_method = :smtp
  # config.action_mailer.delivery_method = :smtp
  ActionMailer::Base.smtp_settings = { 
- 	:tls => true, 
+ 	# :tls => true, 
  	:address => 'smtp.gmail.com', 
  	:port => '587', 
  	:domain => 'danogwok.com', 
